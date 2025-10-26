32db5b98249c54d7565ef37791456d2dd1c2628e
shin0112 <wnslcosltimo12@gmail.com>
2025-10-27 03:19:01 +0900

    feat: 싱글톤으로 수정

--- Assets/Scripts/Manager/BackgroundManager.cs
+++ Assets/Scripts/Manager/BackgroundManager.cs
@@ -2,10 +2,18 @@ using UnityEngine;
 
 public class BackGroundManager : MonoBehaviour
 {
+    private static BackGroundManager _instance;
+    public static BackGroundManager Instance => _instance;
+
     [Header("Background References")]
     [SerializeField] private GameObject _home;
 
     private GameObject _current;
+
+    private void Awake()
+    {
+        _instance = this;
+    }
 
     private void Start()
     {
