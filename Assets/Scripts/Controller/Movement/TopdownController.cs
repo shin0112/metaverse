4fddf1f1c09ce579818da444fb911e7a234f7f2b
shin0112 <wnslcosltimo12@gmail.com>
2025-10-27 00:25:03 +0900

    feat: handle action 메서드 추가

--- Assets/Scripts/Controller/Movement/TopdownController.cs
+++ Assets/Scripts/Controller/Movement/TopdownController.cs
@@ -1,5 +1,11 @@
+using UnityEngine;
+
 public class TopdownController : BaseController
 {
-
-
+    protected override void HandleAction()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        movementDirection = new Vector2(horizontal, vertical).normalized;
+    }
 }
