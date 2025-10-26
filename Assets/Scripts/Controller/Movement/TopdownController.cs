54967e4e499a62ec04b4d0360bbbf2153d45c459
shin0112 <wnslcosltimo12@gmail.com>
2025-10-27 00:32:44 +0900

    feat: player z rotation 제거, movement 정상화

--- Assets/Scripts/Controller/Movement/TopdownController.cs
+++ Assets/Scripts/Controller/Movement/TopdownController.cs
@@ -8,4 +8,10 @@ public class TopdownController : BaseController
         float vertical = Input.GetAxisRaw("Vertical");
         movementDirection = new Vector2(horizontal, vertical).normalized;
     }
+
+    protected override void Movement(Vector2 direction)
+    {
+        direction = direction * 5;
+        _rigidbody.velocity = direction;
+    }
 }
