1c75ddb26664b4c409f936c837060f8bcec839c1
shin0112 <wnslcosltimo12@gmail.com>
2025-10-27 01:43:31 +0900

    fix: 카메라 이동 y축 추가

--- Assets/Scripts/Controller/Camera/FollowCameraController.cs
+++ Assets/Scripts/Controller/Camera/FollowCameraController.cs
@@ -4,12 +4,14 @@ public class FollowCameraController : MonoBehaviour
 {
     public Transform target;
     float offsetX;
+    float offsetY;
 
     private void Start()
     {
         if (target == null) return;
 
         offsetX = transform.position.x - target.position.x;
+        offsetY = transform.position.y - target.position.y;
     }
 
     private void Update()
@@ -18,6 +20,7 @@ public class FollowCameraController : MonoBehaviour
 
         Vector3 pos = transform.position;
         pos.x = target.position.x + offsetX;
+        pos.y = target.position.y + offsetY;
         transform.position = pos;
     }
 }
