30ce264bd6983c1f26f62fc45a1acfccc57cc5a7
shin0112 <wnslcosltimo12@gmail.com>
2025-10-27 20:39:23 +0900

    feat: 드론 기본 모드에서는 충돌 안하게 하기

--- Assets/Scripts/Controller/DroneMovementMode.cs
+++ Assets/Scripts/Controller/DroneMovementMode.cs
@@ -5,12 +5,15 @@ public class DroneMovementMode : MonoBehaviour
     private ScrollController _scrollController;
     private DroneFollowController _droneFollowController;
 
+    private Collider2D _collider;
+
     public bool IsMiniGameMode { get; private set; }
 
     public void Start()
     {
         _scrollController = GetComponent<ScrollController>();
         _droneFollowController = GetComponent<DroneFollowController>();
+        _collider = GetComponent<Collider2D>();
 
         SetDefaultMode();
     }
@@ -20,6 +23,12 @@ public class DroneMovementMode : MonoBehaviour
         IsMiniGameMode = true;
         _scrollController.enabled = true;
         _droneFollowController.enabled = false;
+
+        if (_collider != null)
+        {
+            _collider.isTrigger = false;
+        }
+
         Debug.Log("드론 미니게임 모드 활성화");
     }
 
@@ -28,6 +37,12 @@ public class DroneMovementMode : MonoBehaviour
         IsMiniGameMode = false;
         _scrollController.enabled = false;
         _droneFollowController.enabled = true;
+
+        if (_collider != null)
+        {
+            _collider.isTrigger = true;
+        }
+
         Debug.Log("드론 기본 모드 활성화");
     }
 }
