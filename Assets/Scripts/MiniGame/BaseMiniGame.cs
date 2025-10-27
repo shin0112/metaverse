9043d040531840d1781af6de7826adab5c89681f
shin0112 <wnslcosltimo12@gmail.com>
2025-10-27 14:11:27 +0900

    feat: base mini game 만들기

---
+++ Assets/Scripts/MiniGame/BaseMiniGame.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class BaseMiniGame : MonoBehaviour
+{
+    [SerializeField] protected BaseController _controller;
+
+    protected bool _isReady = false;
+    protected bool _isStart = false;
+
+    public virtual void Init()
+    {
+        _isReady = true;
+        _isStart = false;
+        OnReady();
+    }
+
+    protected virtual void Update()
+    {
+        if (_isReady && !_isStart && CheckStartInput())
+        {
+            StartGame();
+        }
+
+        if (_isStart)
+            OnPlaying();
+    }
+
+    protected virtual bool CheckStartInput()
+    {
+        // space or 마우스 좌클릭
+        return Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0);
+    }
+
+    protected virtual void StartGame()
+    {
+        _isReady = false;
+        _isStart = true;
+        OnStart();
+    }
+
+    protected abstract void OnReady();
+    protected abstract void OnStart();
+    protected abstract void OnPlaying();
+    protected abstract void OnExit();
+}
