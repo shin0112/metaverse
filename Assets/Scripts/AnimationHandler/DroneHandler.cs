1e6350ae9e09c361db434e16a6d44b983af4c1b0
shin0112 <wnslcosltimo12@gmail.com>
2025-10-27 15:47:18 +0900

    feat: 드론 파괴 로직 작성
    - 드론 충돌 감지 -> damage 애니메이션 -> die 애니메이션

--- Assets/Scripts/AnimationHandler/DroneHandler.cs
+++ Assets/Scripts/AnimationHandler/DroneHandler.cs
@@ -1,18 +1,31 @@
+using System.Collections;
 using UnityEngine;
 
 public class DroneHandler : MonoBehaviour
 {
+    private static readonly int IsDamage = Animator.StringToHash("IsDamage");
     private static readonly int IsDead = Animator.StringToHash("IsDead");
 
-    protected Animator animator;
+    private Animator _animator;
 
     public void Awake()
     {
-        animator = GetComponentInChildren<Animator>();
+        _animator = GetComponentInChildren<Animator>();
     }
 
     public void Die()
     {
-        animator.SetBool(IsDead, true);
+        _animator.SetBool(IsDamage, true);
+        StartCoroutine(TransitionToDead(1f)); // 1ÃÊ µÚ »ç¸Á
+    }
+
+    private IEnumerator TransitionToDead(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        _animator.SetBool(IsDamage, false);
+        _animator.SetBool(IsDead, true);
+
+        Debug.Log("µå·Ð ÆÄ±«");
     }
 }
--- Assets/Scripts/Controller/Movement/ScrollController.cs
+++ Assets/Scripts/Controller/Movement/ScrollController.cs
@@ -4,22 +4,33 @@ public class ScrollController : BaseController
 {
     [SerializeField] private float _flapPower = 5f;
 
+    private DroneHandler _animator;
+    private bool _isDead;
+
     protected override void Start()
     {
         base.Start();
         _rigidbody.gravityScale = 1f;
+        _animator = GetComponent<DroneHandler>();
+        _isDead = false;
     }
 
     public void Flap(float power)
     {
         _rigidbody.velocity = Vector2.up * power;
-
-        // 드론 애니메이션
     }
 
     protected override void Rotate()
     {
         float angle = Mathf.Clamp(_rigidbody.velocity.y * 5f, -45f, 45f);
         transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (_isDead) return;
+
+        _animator.Die();
+        _isDead = true;
     }
 }
