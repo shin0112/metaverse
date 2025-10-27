d4b567fdb3f4b8b6577f41dedad959abe6ff381f
shin0112 <wnslcosltimo12@gmail.com>
2025-10-27 18:15:08 +0900

    log: 화면 비활성화 & 활성화 디버그 추가

--- Assets/Scripts/Manager/EnvironmentManager.cs
+++ Assets/Scripts/Manager/EnvironmentManager.cs
@@ -25,9 +25,11 @@ public class EnvironmentManager : MonoBehaviour
     {
         if (_current != null)
         {
+            Debug.Log($"기존 화면 비활성화: {_current.name}");
             _current.SetActive(false);
         }
 
+        Debug.Log($"새로운 화면 활성화: {bg.name}");
         bg.SetActive(true);
         _current = bg;
     }
@@ -39,7 +41,9 @@ public class EnvironmentManager : MonoBehaviour
 
     public GameObject EnterGetFruit()
     {
+        Debug.Log($"[EnvironmentManager] _getFruit: {_getFruit}, activeSelf={_getFruit.activeSelf}");
         SetEnvironment(_getFruit);
+        Debug.Log($"[EnvironmentManager] after SetActive: activeSelf={_getFruit.activeSelf}");
         return _getFruit;
     }
 }
