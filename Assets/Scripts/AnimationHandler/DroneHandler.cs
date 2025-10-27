0a803a7eda92589831a0229c37dacdbad9290648
shin0112 <wnslcosltimo12@gmail.com>
2025-10-27 15:30:00 +0900

    feat: 드론 애니메이션 연결하기

--- Assets/Scripts/AnimationHandler/DroneHandler.cs
+++ Assets/Scripts/AnimationHandler/DroneHandler.cs
@@ -2,6 +2,17 @@ using UnityEngine;
 
 public class DroneHandler : MonoBehaviour
 {
-    private static readonly int IsDeath = Animator.StringToHash("IsDeath");
+    private static readonly int IsDead = Animator.StringToHash("IsDead");
 
+    protected Animator animator;
+
+    public void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    public void Die()
+    {
+        animator.SetBool(IsDead, true);
+    }
 }
